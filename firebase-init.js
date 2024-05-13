df6b87bd46e4019c1285970c9d923bdecc120852
Samellyne777 <133653744+Samellyne777@users.noreply.github.com>
2024-05-13 18:11:20 -0300

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBpo8YjjoeufzFtI1_1WCT4qcdvktbWrdA",
+  authDomain: "cs-connections-e31a7.firebaseapp.com",
+  projectId: "cs-connections-e31a7",
+  storageBucket: "cs-connections-e31a7.appspot.com",
+  messagingSenderId: "779912699994",
+  appId: "1:779912699994:web:45d7f02705c09fb3293863",
+  measurementId: "G-TS7JYLKLBD"
+};
+firebase.initializeApp(firebaseConfig);
